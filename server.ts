00d10e7f2bf45467edce93230e4df2608d365978
LeonLav77 <leonlav77@gmail.com>
2024-12-30 00:44:41 +0100

    CORS headers

--- server.ts
+++ server.ts
@@ -5,6 +5,23 @@ import usersRouter from './app/routes/users';
 const app: Application = express();
 const PORT: number = 3000;
 
+// CORS Middleware
+app.use(function(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+        res.setHeader('Access-Control-Max-Age', '3600');
+        res.sendStatus(200);
+        next();
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    next();
+});
+
 // Middleware to parse JSON
 app.use(express.json());
 
